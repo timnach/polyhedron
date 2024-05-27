a2652d9d773aaa5017ac6c138094eedee938ca75
timnach <165675766+timnach@users.noreply.github.com>
2024-05-27 12:05:02 +0300

    Update run_shadow.py

--- run_shadow.py
+++ run_shadow.py
@@ -15,8 +15,8 @@ try:
         polyedr.draw(tk)
         delta_time = time() - start_time
         print(f"Изображение полиэдра '{name}' заняло {delta_time} сек.")
-        print(f"Сумма площадей граней, центр и хотя бы одна точка"
-              f"которых являются «хорошими» точками: {polyedr.area:6.2f}")
+        print(f"сумма площадей граней, центр и хотя бы одна"
+              f" вершина которых — «хорошие» точки: {polyedr.area:6.2f}")
         input("Hit 'Return' to continue -> ")
 except (EOFError, KeyboardInterrupt):
     print("\nStop")
